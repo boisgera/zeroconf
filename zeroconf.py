4ff624fd311c793ff7ce231de99349a6389553fd
Sébastien Boisgerault <Sebastien.Boisgerault@mines-paristech.fr>
2012-04-18 18:33:58 +0200

    service registration implemented

--- zeroconf.py
+++ zeroconf.py
@@ -2,14 +2,16 @@
 # coding: utf-8
 
 """
-Simple/Pythonic Zeroconf Publish/Discover
+Simple/Pythonic Zeroconf Service Finder/Publisher
 """
 
 __author__ = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>"
 __version__ = None
 
 # Python 2.7 Standard Library
+import pipes
 import re
+import subprocess
 import sys
 
 # Third-Party Libraries
@@ -19,6 +21,8 @@ if sys.platform.startswith("linux"):
     if not host.which("avahi-browse"):
         raise ImportError("unable to find avahi command-line tools")
 
+# Service Search
+# ------------------------------------------------------------------------------
 def find(name=None, type=None, domain="local"):
     """
     Find available zeroconf services
@@ -42,17 +46,17 @@ def find(name=None, type=None, domain="local"):
     info = {}
     for result in results:
         if result[0] == "=":
-            symbol, _, ip_version, _name, _type, _domain, \
+            symbol, _, ip_version, name_, type_, domain_, \
             hostname, address, port, txt = result
-            _name = decode(_name)
+            name_ = decode(name_)
             if ip_version == "IPv4":
-                info[(_name, _type, _domain)] = {"hostname": hostname,
+                info[(name_, type_, domain_)] = {"hostname": hostname,
                                                  "address" : address ,
                                                  "port"    : port    ,
                                                  "txt"     : txt     }
     def name_match(service):
-        _name, _, _ = service
-        return (name is None or _name == name)
+        name_, _, _ = service
+        return (name is None or name_ == name)
 
     filtered_info = [item for item in info.items() if name_match(item[0])]
     return dict(filtered_info)
@@ -84,6 +88,33 @@ def decode(text):
             return other[1:]
 
     return re.sub(r"(\\\d\d\d)|(\\.)", replace, text)
+
+# Service Registration
+# ------------------------------------------------------------------------------
+_publishers = {}
+
+def register(name, type, port):
+    port = str(port)
+    if (name, type, port) in _publishers:
+        raise RuntimeError("service already registered")
+    else:
+        args = ["avahi-publish", "-s", name, type, port]
+        publisher = subprocess.Popen(args, stderr=subprocess.PIPE, \
+                                           stdout=subprocess.PIPE)
+        _publishers[(name, type, port)] = publisher
+
+def unregister(name=None, type=None, port=None):
+    if port:
+        port = str(port)
+    pids = []
+    for name_, type_, port_ in _publishers:
+        if (name is None or name_ == name) and \
+           (type is None or type_ == type) and \
+           (port is None or port_ == port):
+           pids.append((name_, type_, port_))
+    for pid in pids:
+        _publishers[pid].kill()
+        del _publishers[pid]
 
 if __name__ == "__main__":
     import doctest
