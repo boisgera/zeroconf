ada9325236446402ca7fbc3552ae7397ff0eae39
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2016-05-06 11:03:08 +0200

    bugfix

--- zeroconf.py
+++ zeroconf.py
@@ -8,7 +8,7 @@ Simple/Pythonic Zeroconf Service Search/Registration
 __author__ = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>"
 __license__ = "MIT License"
 __url__ = "https://github.com/boisgera/zeroconf" 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 
 # Python 2.7 Standard Library
 import atexit
